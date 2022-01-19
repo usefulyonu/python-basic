b60715bbf8d73abba729fa54095f83663832014e
usefulyonu <98030290+usefulyonu@users.noreply.github.com>
2022-01-19 22:31:27 +0900

    practice

---
+++ 1.py
@@ -0,0 +1,27 @@
+import turtle as t
+t.shape('turtle')
+#사각형 그리기
+for x in range(1,15+1):
+    t.fd(62)
+    t.lt(90)
+    t.fd(24)
+    t.rt(54)
+    t.rt(26)
+    t.fd(33)
+    t.lt(89)
+    t.fd(1)
+    t.rt(13)
+    t.fd(8)
+    t.speed(100000)
+    
+    
+
+
+
+
+
+
+
+
+
+
